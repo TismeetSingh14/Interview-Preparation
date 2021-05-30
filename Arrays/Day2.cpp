e2a9f0c5b6aab494721c2670e3da99ae90f8a0d7
Gurpreet Kaur <tismeet14sh@gmail.com>
2021-05-30 17:31:37 +0530

    code

--- Arrays/Day2.cpp
+++ Arrays/Day2.cpp
@@ -145,10 +145,70 @@ int majorityElement(vector<int>& arr) {
 
 // QUESTION 8
 // MAJORITY ELEMENT II LEETCODE
+vector<int> majorityElementII(vector<int> &arr) {
+    int val1 = arr[0];
+    int count1 = 1;
+    int val2 = arr[0];
+    int count2 = 0;
 
+    for(int i = 1; i < arr.size(); i++) {
+        if(val1 == arr[i] || val2 == arr[i]) {
+            if(val1 == arr[i])
+                count1++;
+            else
+                count2++;
+            continue;
+        }
+        if(count1 == 0) {
+            val1 = arr[i];
+            count1++;
+            continue;
+        }
+
+        if(count2 == 0) {
+            val2 = arr[i];
+            count2++;
+            continue;
+        }
+
+        count1--;
+        count2--;
+    }
+
+    int freq = arr.size()/3 + 1;
+    int c1 = 0;
+    int c2 = 0;
+    for(int i = 0; i < arr.size(); i++) {
+        if(arr[i] == val1)
+            c1++;
+        if(arr[i] == val2)
+            c2++;
+    }
+
+    vector<int> ans;
+    if(c1 >= freq) 
+        ans.push_back(val1);
+    if(c2 >= freq && val1 != val2) 
+        ans.push_back(val2);
+    return ans;
+}
 
 // QUESTION 9
 // MAJORITY ELEMENT GENERAL GFG
+vector<int> majorityElementGeneral(vector<int> &arr, int k) {
+    int freq = arr.size()/k + 1;
+    unordered_map<int, int> map;
+    vector<int> ans;
+
+    for(int i = 0; i < arr.size(); i++)
+        map[arr[i]]++;
+
+    for(auto k:map)
+        if(k.second >= freq)
+            ans.push_back(k.first);
+
+    return ans;
+}
 
 // QUESTION 10
 // MAX CHUNKS TO MAKE ARRAY SORTED
@@ -167,7 +227,114 @@ int maxChunks(vector<int> &arr) {
 // QUESTION 11
 // MAX CHUNKS TO MAKE ARRAY SORTED II LEETCODE
 int maxChunksII(vector<int> &arr) {
+    vector<int> prefixMax(arr.size(), 0);
+    prefixMax[0] = arr[0];
+    for(int i = 1; i <arr.size(); i++) {
+        prefixMax[i] = max(prefixMax[i-1], arr[i]);
+    }
+
+    vector<int> suffixMin(arr.size(), 0);
+    suffixMin[arr.size() - 1] = arr[arr.size() - 1];
+    for(int i = arr.size() - 2; i >= 0; i--) {
+        suffixMin[i] = min(suffixMin[i + 1], arr[i]);
+    }
+
+    int chunks = 0;
+    for(int i = 0; i < arr.size() - 1; i++) {
+        if(prefixMax[i] <= suffixMin[i+1]) 
+            chunks++;
+    }
+
+    return chunks+1;
+}
+
+// QUESTION 12
+// MAXIMUM PRODUCT OF THREE NUMBERS LEETCODE
+int maxProduct(vector<int> &nums) {
+    int max1 = -1e8;
+    int max2 = -1e8;
+    int max3 = -1e8;
+
+    for(int n:nums) {
+        if(n > max1) {
+            max3 = max2;
+            max2 = max1;
+            max1 = n;
+        }
+
+        else if(n > max2) {
+            max3 = max2;
+            max2 = n;
+        }
+
+        else if(n > max3) {
+            max3 = n;
+        }
+    }
+
+    int min1 = 1e8;
+    int min2 = 1e8;
+
+    for(int n:nums) {
+        if(n < min1) {
+            min2 = min1;
+            min1 = n;
+        }
+
+        else if(n < min2) {
+            min2 = n;
+        }
+    }
+
+    return max(min2*min1*max1, max1*max2*max3);
+}
+
+// QUESTION 13
+// LARGEST NUMBER AT LEAST TWICE OF OTHERS LEETCODE
+int dominantIndex(vector<int>& nums) {
+    if(nums.size() < 2)
+        return 0;
+    int max2 = INT_MIN;
+    int max1 = INT_MIN;
+    int idx2 = -1;
+    int idx1 = -1;
+    for(int n: nums) {
+        if(n > max1) {
+            max2 = max1;
+            max1 = n;
+        }
+        
+        else if(n > max2) {
+            max2 = n;
+        }
+    }
+    if(max2*2 <= max1) {
+        for(int i = 0; i < nums.size(); i++)
+            if(max1 == nums[i])
+                return i;
+    }
     
+    return -1;
+}
+
+// QUESTION 14
+// PRODUCT OF ARRAY EXCEPT SELF LEETCODE 
+vector<int> productExceptSelf(vector<int>& arr) {
+    vector<int> ans(arr.size(), 1);
+
+    ans[0] = arr[0];
+    for(int i = 1; i < arr.size(); i++)
+        ans[i] = ans[i-1]*arr[i];
+
+    int suffProd = arr[arr.size() - 1];
+    ans[ans.size() - 1] = ans[ans.size() - 2];
+    for(int i = arr.size() - 2; i >= 1; i--) {
+        ans[i] = ans[i-1]*suffProd;
+        suffProd *= arr[i];
+    }
+    
+    ans[0] = suffProd;
+    return ans;
 }
 
 int main(int argc, char** argv) {
--- Arrays/Day3.cpp
+++ Arrays/Day3.cpp
@@ -5,6 +5,54 @@
 #include <algorithm>
 using namespace std;
 
+// QUESTION 1
+// NUMBER OF SUBARRAYS WITH BOUNDED MAXIMUM LEETCODE
+int maxSubarrays(vector<int> &arr, int l, int r) {
+    int ans = 0;
+    int i = 0;
+    int j = 0;
+    int prevValCount = 0;
+
+    while(i < arr.size() && j < arr.size()) {
+        if(arr[j] >= l && arr[j] <= r) {
+            ans += (j - i + 1);
+            prevValCount = (j - i + 1);
+            j++;
+        }
+
+        else if(arr[j] < l) {
+            ans += prevValCount;
+            j++;
+        }
+
+        else if(arr[j] > r) {
+            prevValCount = 0;
+            i = j + 1;
+            j++;
+        }
+    }
+
+    return ans;
+}
+
+// QUESTION 2
+// MAXIMUM SUM SUBARRAY LEETCODE
+// KADANES ALGORITHM
+int maxSubarraySum(vector<int> &arr) {
+    int cmax = 0;
+    int omax = -1e9;
+
+    for(int i = 0; i < arr.size(); i++) {
+        if(cmax >= 0)
+            cmax += arr[i];
+        else
+            cmax = arr[i];
+        omax = max(omax, cmax);
+    }
+
+    return omax;
+}
+
 int main(int argc, char** argv) {
     
 }
